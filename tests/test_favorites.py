7cb6237ed90d2f38aabe77696dd5469a66551cdc
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-10 11:34:42 -0500

    add two more tests

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -5,3 +5,25 @@ def test_starting_values():
     favorites_contract = deploy_favorites()
     starting_number = favorites_contract.retrieve()
     assert 42 == starting_number
+
+
+def test_updating_values():
+    favorites_contract = deploy_favorites()
+    favorites_contract.store(100)
+    updated_number = favorites_contract.retrieve()
+    assert 100 == updated_number
+
+
+def test_can_add_people():
+    # arrange
+    new_person = "Alice"
+    new_favorite_number = 15
+    favorites_contract = deploy_favorites()
+
+    # act
+    favorites_contract.add_person(new_person, new_favorite_number)
+
+    # assert
+    (number, name) = favorites_contract.list_of_people(0)
+    assert new_person == name
+    assert new_favorite_number == number
